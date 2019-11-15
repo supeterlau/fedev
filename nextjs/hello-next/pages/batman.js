9d078da39258b4a019b970d143993ce85e4c2608
bernielxy <xiangyuliu2016@outlook.com>
2019-11-15 13:07:06 +0800

    [next.js] fetch data from api

---
+++ nextjs/hello-next/pages/batman.js
@@ -0,0 +1,31 @@
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = props => (
+  <Layout>
+    <h1>Batman TV shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function () {
+  const apiURL = 'https://api.tvmaze.com/search/shows?q=batman'
+  const res = await fetch(apiURL)
+  const data = await res.json()
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
+}
+
+export default Index
