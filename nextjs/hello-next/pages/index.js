63ada9a98eec7828265783baaa1faea1748adc8c
bernielxy <xiangyuliu2016@outlook.com>
2019-11-15 12:24:31 +0800

    [next.js] refactor index and about pages

--- nextjs/hello-next/pages/index.js
+++ nextjs/hello-next/pages/index.js
@@ -1,10 +1,21 @@
-import Header from '../components/Header'
+import Layout from '../components/MyLayout'
 
-const Index = () => (
-  <div>
-    <Header />
-    <p>Hello Next.js</p>
-  </div>
-)
+export default function Index() {
+  return (
+    <Layout>
+      <p>Hello Next.js</p>
+    </Layout>
+  )
+}
 
-export default Index;
+
+// import Header from '../components/Header'
+
+// const Index = () => (
+//   <div>
+//     <Header />
+//     <p>Hello Next.js</p>
+//   </div>
+// )
+
+// export default Index;
