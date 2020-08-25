c44138ba83f92c4d13d5e9a8f47ff3441a2012c1
PeterLau <superpeterlau@outlook.com>
2020-08-25 16:13:50 +0800

    flutter: add ListView; convert 4 spaces to 2 spaces

--- e8t/flutter/tour/lib/00_get_started/main.dart
+++ e8t/flutter/tour/lib/00_get_started/main.dart
@@ -2,32 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 class MainApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
-    return MaterialApp(
-        title: 'Flutter',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Welcome to Flutter'),
-            ),
-            body: Center(
-              // child: Text('Hello Flutter'),
-              // child: Text(wordPair.asPascalCase)
-              child: RandomWords(),
-            )));
-  }
+ @override
+ Widget build(BuildContext context) {
+  // final wordPair = WordPair.random();
+  return MaterialApp(
+    title: 'Name Generator',
+    home: RandomWords(),
+  );
+ }
 }
 
 class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
+ @override
+ _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
+ final _suggestions = <WordPair>[];
+ final _biggerFont = TextStyle(fontSize: 18.0);
+
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Name Generator'),
+    ),
+    body: _buildSuggestions(),
+  );
+ }
+
+ Widget _buildSuggestions() {
+  return ListView.builder(
+    padding: EdgeInsets.all(16.0),
+    itemBuilder: (context, i) {
+     if (i.isOdd) return Divider();
+
+     final index = i ~/ 2;  // 计算减去分隔线后实际数量
+     if (index >= _suggestions.length) {
+       _suggestions.addAll(generateWordPairs().take(10));
+     }
+     return _buildRow(_suggestions[index]);
+    }
+  );
+ }
+
+ Widget _buildRow(WordPair pair) {
+  return ListTile(
+    title: Text(
+      pair.asPascalCase,
+      style: _biggerFont,
+    )
+  );
+ }
 }
