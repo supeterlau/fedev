c12c10bc6c0e47a6fa60183901ba3f7300eae2bd
PeterLau <superpeterlau@outlook.com>
2021-04-08 10:47:05 +0800

    reactnative: basic: add button

--- reactnative/basic/src/App.js
+++ reactnative/basic/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import logo from "../assets/logo.png";
 
 export default function AppComp() {
@@ -15,6 +15,14 @@ export default function AppComp() {
       />
       <Text style={styles.text}>Share a photo with a friend</Text>
       <Text style={styles.instructions}>Press button below</Text>
+      <TouchableOpacity
+        onPress={() => alert("Cool")}
+        // style={{ backgroundColor: "blue" }}
+        style={styles.button}
+      >
+        {/* <Text style={{ fontSize: 20, color: "#fff" }}>Pick a photo</Text> */}
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -39,5 +47,14 @@ const styles = StyleSheet.create({
     color: "#888",
     fontSize: 18,
     marginHorizontal: 35,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 15,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
