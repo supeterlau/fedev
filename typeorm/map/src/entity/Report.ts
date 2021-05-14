2ba1d4149c1523c2ec7ec2ee287a70cacbaae720
PeterLau <superpeterlau@outlook.com>
2021-05-14 16:33:07 +0800

    basic orm

---
+++ typeorm/map/src/entity/Report.ts
@@ -0,0 +1,60 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+class Report {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public location: string;
+
+  @Column({ nullable: true })
+  public coords: string;
+
+  @Column({ type: "timestamptz" })
+  public happenAt: Date;
+
+  @Column({ type: "timestamptz", default: new Date() })
+  public reportAt: Date;
+
+  @Column()
+  public overview: string;
+
+  @Column({ type: "text", nullable: true })
+  public detail: string;
+
+  // 事件类别
+  // 提供选项
+  @Column()
+  public category: string;
+
+  // people relationship
+  // 提供选项
+  @Column()
+  public relationship: string;
+
+  @Column({ type: "text", nullable: true })
+  public theOtherInfo: string;
+
+  // reporter info
+  @Column()
+  public reporterName: string;
+
+  // 提供选项
+  @Column()
+  public reporterRole: string;
+
+  @Column()
+  public contact: string;
+
+  // 信息来源
+  // 提供选项
+  @Column({ default: "submit" })
+  public source: string;
+
+  // 是否核时
+  @Column()
+  public checked: boolean;
+}
+
+export default Report;
