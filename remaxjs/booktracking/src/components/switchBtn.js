c6a499d7e9d33405fd58054468903a0e6bd9f852
PeterLau <superpeterlau@outlook.com>
2021-08-14 10:55:56 +0800

    little react

--- remaxjs/booktracking/src/components/switchBtn.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Button } from 'remax/wechat';
-
-const style = {
-  width: '10px'
-}
-export default (props) => <Button style={style}>{props.children} </Button>
