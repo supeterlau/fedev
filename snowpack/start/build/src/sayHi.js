b547bda8a55b42a06d59d5cb84b9b4b6aed3db7b
PeterLau <superpeterlau@outlook.com>
2021-10-18 23:50:41 +0800

    snowpack start app

---
+++ snowpack/start/build/src/sayHi.js
@@ -0,0 +1,10 @@
+/*
+ * sayHi.js
+ * Copyright (C) 2021 Peter Lau <superpeterlau@outlook.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+export function sayHi() {
+  console.log("Say Hi loudly");
+}
